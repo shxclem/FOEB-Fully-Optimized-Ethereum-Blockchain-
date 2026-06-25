59a63882a505f213a14a1b2d4588330cb5ab769f
shxclem <105485646+shxclem@users.noreply.github.com>
2026-06-25 09:40:19 +0100

    Create MessageMessenger.sol

---
+++ MessageMessenger.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MessageManager {
+
+    struct Message {
+        uint256 id;
+        address sender;
+        address receiver;
+    }
+
+    uint256 public index;
+    mapping(address => Message[]) public sent_by;
+    mapping(address => Message[]) public received_by;
+    mapping(address => bytes) public publicKeys;
+
+    event NewTx(
+                uint256 indexed tx_id,
+                address indexed sender,
+                address indexed receiver
+                );
+
+    constructor() {
+        index = 0;
+    }
+    
+    function send_message(address receiver, bytes calldata publicKey) public {
+        if (publicKeys[msg.sender].length == 0) {
+            publicKeys[msg.sender] = publicKey;
+        }
+
+        index++;
+
+        emit NewTx(index, msg.sender, receiver);
+        sent_by[msg.sender].push(Message(index, msg.sender, receiver));
+        received_by[receiver].push(Message(index, msg.sender, receiver));
+    }
+
+    function sent_by_counter(address sender) external view returns (uint) {
+        return sent_by[sender].length;
+    }
+
+    function received_by_counter(address receiver) external view returns (uint) {
+        return received_by[receiver].length;
+    }
+}
